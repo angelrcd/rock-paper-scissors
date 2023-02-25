f278a4271b40da6173f9a1577f87ad8a681ca53f
Angel Romero <mrangelrcd@gmail.com>
2023-02-25 18:03:31 +0100

    game result is displayed

--- main.js
+++ main.js
@@ -5,11 +5,15 @@ const cpuScoreDOM = document.querySelector(".cpu-score");
 const playerScoreDOM = document.querySelector(".player-score");
 const playerChoiceButtons = document.querySelectorAll(".player-choice-container button");
 const playButton = document.querySelector(".begin-match-button")
+const resultDisplay = document.querySelector(".result")
+const countdownDisplay = document.querySelector(".countdown")
 
 // Event Listeners
 playerChoiceButtons.forEach((choiceButton, index) => {
   choiceButton.addEventListener("click", () => setPlayerChoice(index))
 })
+
+playButton.addEventListener("click", beginMatch)
 
 // States
 let playerChoice = null;
@@ -30,4 +34,41 @@ function setPlayerChoice(choice) {
  */
 function generateCPUChoice() {
   cpuChoice = Math.floor(Math.random() * 3);
+}
+
+/**
+ * Display on the screen the result of the current game
+ * @param {number} gameResult 
+ */
+function displayResult(gameResult){
+  let result = ""
+  switch(gameResult){
+    case -1:
+      result = "You lost!"
+      break;
+    case 0:
+      result = "Tie!"
+      break;
+    case 1:
+      result = "You won!"
+      break;
+  }
+
+  resultDisplay.textContent = result;
+  resultDisplay.classList.remove("hidden")
+}
+
+/**
+ * Hides the result in display
+ */
+function hideResult(){
+  resultDisplay.classList.add("hidden")
+}
+
+function beginMatch(){
+  generateCPUChoice()
+  const gameResult = simulateGame(playerChoice, cpuChoice);
+  displayResult(gameResult)
+  matchHistory.push(gameResult)
+  console.log(matchHistory)
 }
