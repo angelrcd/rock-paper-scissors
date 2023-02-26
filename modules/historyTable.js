a2a892fc1ced7cfcc30713888b872d10c48acdc0
Angel Romero <mrangelrcd@gmail.com>
2023-02-26 17:15:41 +0100

    table has text and colors

--- modules/historyTable.js
+++ modules/historyTable.js
@@ -2,9 +2,21 @@ const table = document.querySelector("table")
 
 export default function insertMatch(number, winner){
   const row = table.insertRow()
+  let winnerText;
+  if (winner === 1) {
+    winnerText = "You"
+    row.classList.add("winner-row");
+  } else if (winner === -1)  {
+    winnerText = "CPU"
+    row.classList.add("loser-row");
+  } else {
+    winnerText = "Tie"
+  }
+
 
   const firstCell = row.insertCell();
   firstCell.textContent = number
+
   const secondCell = row.insertCell();
-  secondCell.textContent = winner
+  secondCell.textContent = winnerText;
 }
